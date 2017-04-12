e18e684f74e6a07c60401ab1a31257d7c8ee5bf0
chimame <rito.tamata@gmail.com>
2017-04-13 01:08:51 +0900

    scenario 11

--- client/app/components/MessageForm/index.js
+++ client/app/components/MessageForm/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
 class MessageForm extends Component {
+  onSubmit() {
+    const { actions } = this.props
+    actions.addMessage(this.textMessage.value)
+  }
+
   render() {
     return (
       <div>
-        <textarea rows="4" cols="40" />
-        <input type="button" value="登録" />
+        <textarea rows="4" cols="40" ref={(input) => { this.textMessage = input }} />
+        <input type="button" value="登録" onClick={this.onSubmit.bind(this)} />
       </div>
     )
   }
--- client/app/containers/Chat/index.js
+++ client/app/containers/Chat/index.js
@@ -16,7 +16,7 @@ class App extends Component {
     return (
       <div>
         <MessageList messages={chat} />
-        <MessageForm />
+        <MessageForm actions={actions} />
       </div>
     )
   }
