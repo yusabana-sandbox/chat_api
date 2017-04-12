5b97d6fdc5bc0c7ad9bddba1014445958d4378cd
chimame <rito.tamata@gmail.com>
2017-04-13 00:38:05 +0900

    scenario 6

---
+++ client/app/components/MessageForm/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MessageForm extends Component {
+  render() {
+    return (
+      <div>
+        <textarea rows="4" cols="40" />
+      </div>
+    )
+  }
+}
+
+export default MessageForm
