180257975704828aaa6865640bf9cc70c10fcfc9
chimame <rito.tamata@gmail.com>
2017-04-14 22:23:11 +0900

    scenario 18

--- client/app/reducers/Chat.js
+++ client/app/reducers/Chat.js
@@ -1,7 +1,18 @@
 import { combineReducers } from 'redux'
-import { ADD_MESSAGE, CREATE_MESSAGE_LIST } from '../constants/chat'
+import {
+  ADD_MESSAGE,
+  CREATE_MESSAGE_LIST,
+  TOGGLE_DISPLAY_MESSAGE
+} from '../constants/chat'
 
 export const chatInitialState = [{"id":2,"text":"message2"},{"id":1,"text":"message1"}]
+
+const toggleMessage = (id, message) => {
+  if (id === message.id) {
+    message.displayState = !message.displayState
+  }
+  return message
+}
 
 const chat = (state = '', action) => {
   switch (action.type) {
@@ -14,6 +25,8 @@ const chat = (state = '', action) => {
       const message = action.message
       message['displayState'] = true
       return [ ...state, message ]
+    case TOGGLE_DISPLAY_MESSAGE:
+      return state.map((message) => toggleMessage(action.id, message))
     default:
       return state
   }
