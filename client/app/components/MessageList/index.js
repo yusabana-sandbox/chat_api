31df4a23034c3291f8a5c024228f8bacf9f46bdf
chimame <rito.tamata@gmail.com>
2017-04-14 18:13:08 +0900

    scenario 16

--- client/app/components/MessageList/index.js
+++ client/app/components/MessageList/index.js
@@ -5,9 +5,13 @@ import Message from '../Message'
 class MessageList extends Component {
   componentDidMount() {
     const { actions } = this.props
-    actions.createMessageList(
-      [{"id":3,"text":"message3"},{"id":2,"text":"message2"},{"id":1,"text":"message1"}]
-    )
+    $.ajax({
+      type: 'GET',
+      url: '/api/messages',
+      success: (
+        (data) => actions.createMessageList(data)
+      )
+    })
   }
 
   render() {
