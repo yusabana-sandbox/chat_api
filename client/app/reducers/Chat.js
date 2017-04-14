718516d7f27c10c5dc3605d08526a003cbeaf2a5
chimame <rito.tamata@gmail.com>
2017-04-14 18:08:52 +0900

    scenario 13

--- client/app/reducers/Chat.js
+++ client/app/reducers/Chat.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
-import { ADD_MESSAGE } from '../constants/chat'
+import { ADD_MESSAGE, CREATE_MESSAGE_LIST } from '../constants/chat'
 
 export const chatInitialState = [{"id":2,"text":"message2"},{"id":1,"text":"message1"}]
 
 const chat = (state = '', action) => {
   switch (action.type) {
+    case CREATE_MESSAGE_LIST:
+      return action.messages
     case ADD_MESSAGE:
       return [ ...state, action.message ]
     default:
