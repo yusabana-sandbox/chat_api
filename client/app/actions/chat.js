6007d61aa4c2a82758fbe34bf74b5b43ced97918
chimame <rito.tamata@gmail.com>
2017-04-14 18:09:55 +0900

    scenario 14

--- client/app/actions/chat.js
+++ client/app/actions/chat.js
@@ -1,6 +1,11 @@
-import { ADD_MESSAGE } from '../constants/chat'
+import { ADD_MESSAGE, CREATE_MESSAGE_LIST } from '../constants/chat'
 
 export const addMessage = (message) => ({
   type: ADD_MESSAGE,
   message,
+})
+
+export const createMessageList = (messages) => ({
+  type: CREATE_MESSAGE_LIST,
+  messages,
 })
